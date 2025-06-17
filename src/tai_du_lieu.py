9d803a4e2b3fcef6f43c2e2fecc459a8e0f5c976
nam101nam <dophuongnam1010@gmail.com>
2025-06-17 17:37:17 +0700

    x3

--- src/tai_du_lieu.py
+++ src/tai_du_lieu.py
@@ -1,5 +1,20 @@
 import numpy as np
 import pandas as pd
+import re
+import string
+def lam_sach_email(text):
+    text = str(text).lower()
+    text = re.sub(r"http\S+|www\S+", "", text)  # xóa link
+    text = re.sub(r'\S+@\S+', '', text)  # xóa email
+    text = re.sub(r'\d+', '', text)  # xóa số
+    text = text.translate(str.maketrans('', '', string.punctuation))  # xóa dấu câu
+    text = re.sub(r'\s+', ' ', text).strip()  # xóa khoảng trắng thừa
+    words = text.split() # bỏ stopwords + stemming
+    return ' '.join(words)
+
 df=pd.read_csv('../DuLieu/emails.csv')
-data=df.fillna('')
-data.head()
+df.loc[df['Category']=='spam','Category']=1
+df.loc[df['Category']=='ham','Category']=0
+df['Message'] = df['Message'].apply(lam_sach_email)
+df.to_csv('../DuLieu/nemail.csv')
+print(df)
